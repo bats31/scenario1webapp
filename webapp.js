05b5f8f412ce25d2e58cd262f4423c8059ff085a
bats31 <tobias_schlauch@gmx.de>
2021-08-05 07:30:57 +0200

    scenario1test

--- webapp.js
+++ webapp.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 800;
 http.createServer(function (req, res) {
  
   // HTTP response header - the content will be HTML MIME type
-  res.writeHead(200, { 'Content-Type': "text/html" });
+  res.writeHe(200, { 'Content-Type': "text/html" });
   // Write out the HTTP response body
   res.write("@@TEST@@");
  
